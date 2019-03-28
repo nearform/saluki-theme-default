cd9f031d1165a8178a0e03235d967f5e29475e9c
Chris Draycott-Wheatley <chriswheatley87@gmail.com>
2019-03-28 20:03:49 +0000

    Extract saluki default theme into own component

---
+++ index.js
@@ -0,0 +1,137 @@
+module.exports = {
+  color: {
+    black: "black",
+    white: "white",
+    lighterGray: "#d5d9dc",
+    lightGray: "#9ea6ae",
+    gray: "#495057",
+    darkGray: "#3a4045",
+    darkerGray: "#23262a",
+    lighterRed: "#f9b8b8",
+    lightRed: "#f25959",
+    red: "#f03e3e",
+    darkRed: "#a60d0d",
+    darkerRed: "#470606",
+    lighterBlue: "#bbdaf6",
+    lightBlue: "#61a9ea",
+    blue: "#1c7ed6",
+    darkBlue: "#155d9e",
+    darkerBlue: "#092844",
+    lighterYellow: "#ffe4b3",
+    lightYellow: "#ffc04d",
+    yellow: "#f59f00",
+    darkYellow: "#b37400",
+    darkerYellow: "#4d3200",
+    lighterGreen: "#c5edcc",
+    lightGreen: "#77d587",
+    green: "#37b24d",
+    darkGreen: "#2a883b",
+    darkerGreen: "#123a19",
+    lighterOrange: "#fdd2b5",
+    lightOrange: "#fa9551",
+    orange: "#f76707",
+    darkOrange: "#ae4805",
+    darkerOrange: "#4a1f02",
+    lighterPurple: "#e6c3ee",
+    lightPurple: "#c474d8",
+    purple: "#ae3ec9",
+    darkPurple: "#78278b",
+    darkerPurple: "#33113c"
+  },
+  padding: {
+    none: "0",
+    auto: "auto",
+    small: "1rem",
+    medium: "2rem",
+    large: "4rem"
+  },
+  margin: {
+    none: "0",
+    auto: "auto",
+    small: "1rem",
+    medium: "2rem",
+    large: "4rem"
+  },
+  breakpoint: {
+    notSmall: { min: "576px" },
+    medium: { min: "768px", max: "991px" },
+    large: { min: "992px" }
+  },
+  fontSize: {
+    small: "1rem",
+    medium: "1.25rem",
+    large: "2.5rem"
+  },
+  fontFamily: {
+    sans:
+      "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+    serif: "Georgia, Times, Times New Roman, serif"
+  },
+  fontWeight: {
+    thin: "200",
+    normal: "normal",
+    bold: "700"
+  },
+  width: {
+    fifth: "20%",
+    twoFifths: "40%",
+    threeFifths: "60%",
+    fourFifths: "80%",
+    quarter: "25%",
+    threeQuarters: "75%",
+    third: "33.3333333333%",
+    twoThirds: "66.6666666666%",
+    half: "50%",
+    full: "100%",
+    view: "100vw",
+    auto: "auto"
+  },
+  height: {
+    small: "2rem",
+    medium: "4rem",
+    large: "8rem",
+    quarter: "25%",
+    half: "50%",
+    threeQuarters: "75%",
+    full: "100%",
+    view: "100vh",
+    auto: "auto"
+  },
+  lineHeight: {
+    none: "1",
+    small: "1.25",
+    medium: "1.5",
+    large: "2"
+  },
+  letterSpacing: {
+    small: "-.05em",
+    none: "0",
+    medium: ".025em",
+    large: ".05em"
+  },
+  borderWidth: {
+    none: "none",
+    thin: "1px",
+    medium: "2px",
+    thick: "4px",
+    extraThick: "8px"
+  },
+  borderRadius: {
+    none: 0,
+    small: ".125rem",
+    medium: ".25rem",
+    large: ".5rem",
+    round: "9999px"
+  },
+  opacity: {
+    full: 1,
+    half: 0.5,
+    none: 0,
+    inherit: "inherit"
+  },
+  boxShadow: {
+    small: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+    medium: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
+    large: "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
+  }
+};
